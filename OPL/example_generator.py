35fa77e356760062ed7b536d012277c2dcf4014c
Jérémy Carneau <Jeremy.Carneau@grenoble-inp.org>
2023-06-15 09:50:25 +0200

    feat(): Ajout d'un générateur d'exemple (faisabilité des exemples non assurée)\n Ajout du PL du problème sans capacité avec dispose

---
+++ OPL/example_generator.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+
+"""
+Generate a .dat file following type of lot sizing problem we are working on.
+All the values are integers.
+The realisability of the example isn't guaranteed.
+Enter `./example_generator -h` for help.
+"""
+
+##### Imports
+
+import argparse
+import random as rd
+
+##### Global variables
+
+Ds_mean = 10 # Mean of the remanufactured demand
+Ds_range = 5 # Range of the remanufactured demand
+
+Dn_mean = 15 # Mean of the manufactured demand
+Dn_range = 7 # Range of the manufactured demand
+
+R_mean = 13 # Mean of the number of returns
+R_range = 8 # Range of the number of returns
+
+C_mean = 40 # Mean of the capacity values
+C_range = 3 # Range of the capacity values
+
+f_mean = 15 # Mean of setup cost
+f_range = 8 # Range of setup cost
+
+pn_mean = 4 # Mean of variable manufacturing cost
+pn_range = 3 # Range of variable manufacturing cost
+
+ps_mean = 3 # Mean of variable remanufacturing cost
+ps_range = 2 # Range of variable remanufacturing cost
+
+pr_mean = 2 # Mean of dispose cost
+pr_range = 1 # Range of dispose cost
+
+hn_mean = 3 # Mean of holding cost of manufacturing products
+hn_range = 1 # Range of holding cost of manufacturing products
+
+hs_mean = 2 # Mean of holding cost of remanufacturing products
+hs_range = 1 # Range of holding cost of remanufacturing products
+
+hr_mean = 1 # Mean of holding cost of returns
+hr_range = 0 # Range of holding cost of returns
+
+##### Code
+
+def parse_args():
+    """ Parse flags and store arguments in the global variable `args`."""
+    global args
+    parser = argparse.ArgumentParser(description='Generate a .dat file following type of the chosen lot sizing problem. All the values are integers.')
+    parser.add_argument("-c", "--capacitated", action="store_true", help="The lot sizing problem include capacity")
+    parser.add_argument("-C", action="store_true", help="The capacity is constant (need the capacitated problem)")
+    parser.add_argument("-r", "--returns", action="store_true", help="The lot sizing problem include returns")
+    parser.add_argument("-d", "--dispose", action="store_true", help="The lot sizing problem include dispose")
+    parser.add_argument("-f", "--file", default="example.dat", help="Name of the .dat created file (default : example.dat)")
+    parser.add_argument("-T", type=int, default = 30, help="Length of the horizon (default : 30)")
+    args = parser.parse_args()
+    
+def create_random_list(m, r):
+    """ Returns a list of random integers in the [m - r, m + r] interval."""
+    res = []
+    for _ in range(args.T):
+        res.append(m + rd.randint(-r, r))
+    return res
+
+def generate_example():
+    """ Create the .dat file with the constraints set by the problem."""
+
+    file = open(args.file, "w")
+
+    file.write(f"T = {args.T};\n")
+
+    Dn = create_random_list(Dn_mean, Dn_range)
+    file.write(f"Dn = {Dn};\n")
+
+    Ds = create_random_list(Ds_mean, Ds_range)
+    file.write(f"Ds = {Ds};\n")
+
+    if args.returns:
+        R = create_random_list(R_mean, R_range)
+        file.write(f"R = {R};\n")
+
+    f = create_random_list(f_mean, f_range)
+    file.write(f"f = {f};\n")
+
+    pn = create_random_list(pn_mean, pn_range)
+    file.write(f"pn = {pn};\n")
+
+    ps = create_random_list(ps_mean, ps_range)
+    file.write(f"ps = {ps};\n")
+
+    if args.dispose:
+        pr = create_random_list(pr_mean, pr_range)
+        file.write(f"pr = {pr};\n")
+    
+    hn = create_random_list(hn_mean, hn_range)
+    file.write(f"hn = {hn};\n")
+
+    hs = create_random_list(hs_mean, hs_range)
+    file.write(f"hs = {hs};\n")
+
+    hr = create_random_list(hr_mean, hr_range)
+    file.write(f"hr = {hr};\n")
+
+    if args.capacitated:
+        if args.C:
+            C = [C_mean] * args.T
+        else:
+            C = create_random_list(C_mean, C_range)
+        file.write(f"C = {C};\n")
+
+
+    file.close()
+
+
+def main():
+    """ Main fonction. Only executed when the script is runned (not imported)."""
+    parse_args()
+    
+    generate_example()
+
+    return 1
+
+
+if __name__=="__main__":
+    main()
---
+++ OPL/uncapacitated_dispose/program.mod
@@ -0,0 +1,90 @@
+
+// Uncapacitated with dispose lot sizing problem
+
+/*
+    Data
+*/
+
+int T = ... ; // Length of the horizon
+range periods = 1..T ;
+
+float Dn[periods] = ... ; // Demand of manufactured products
+float Ds[periods] = ... ; // Demand of remanufactured products
+
+float R[periods] = ... ; // Returns (at the beginning of the period)
+
+float f[periods] = ... ; // Fixed joint setup cost
+
+float pn[periods] = ... ; // Variable manufacturing cost
+float ps[periods] = ... ; // Variable remanufacturing cost
+float pr[periods] = ... ; // Variable cost to dispose returns
+
+float hn[periods] = ... ; // Holding cost of manufactured products
+float hs[periods] = ... ; // Holding cost of remanufactured products
+float hr[periods] = ... ; // Holding cost of returns
+
+float M = sum (t in periods) (Dn[t] + Ds[t]); // Max of production
+// TODO : reduce M value to be more efficient
+
+/*
+    Variables declaration
+*/
+
+dvar float+ xn[periods]; // Number of manufactured products
+dvar float+ xs[periods]; // Number of remanufactured products
+dvar float+ xr[periods]; // Number of disposed return products
+
+dvar float+ sn[0..T]; // Inventory of manufactured products
+dvar float+ ss[0..T]; // Inventory of remanufactured products
+dvar float+ sr[0..T]; // Inventory of returns
+
+dvar boolean y[periods]; // y[t] = 1 if production is launched at period t, else 0
+
+/*
+    Objective fonction
+*/
+
+minimize sum (t in periods) ((pn[t] * xn[t] + ps[t] * xs[t]) // Variable costs
+                            + (hn[t] * sn[t] + hs[t] * ss[t] +  hr[t] * sr[t]) // Holding costs
+                            + f[t] * y[t] // Setup cost
+                            + pr[t] * xr[t]); // Dispose cost
+
+/*
+    Constraints
+*/
+
+subject to {
+
+    // Stock is empty at first
+    sn[0] == 0;
+    ss[0] == 0;
+    sr[0] == 0;
+    
+    // We respond to demand
+    forall (t in periods) xn[t] + sn[t - 1] == sn[t] + Dn[t];
+    forall (t in periods) xs[t] + ss[t - 1] == ss[t] + Ds[t];
+
+    // Returns management
+    forall (t in periods) R[t] + sr[t - 1] == xr[t] + sr[t] + xs[t];
+
+    // In order to include production cost
+    forall (t in periods) xn[t] + xs[t] <= M * y[t];
+}
+
+/* Display */
+execute {
+    writeln("Post-traitement: ");
+    writeln("La valeur de l'objectif est de "+cplex.getObjValue());
+
+    writeln("");
+    writeln("Valeurs de production: ");
+    writeln("Produits neufs:          ", xn);
+    writeln("Produits remanufacturés: ", xs);
+    writeln("Dispose:                 ", xr);
+
+    writeln("");
+    writeln("Valeurs de stockage: ");
+    writeln("Stocks neufs:            ", sn);
+    writeln("Stocks remanufacturés:   ", ss);
+    writeln("Stocks retours:          ", sr);
+} 
